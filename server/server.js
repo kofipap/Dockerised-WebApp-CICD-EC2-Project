a355a303ec3240938e9ca1bd91a34a86ebdd60ae
kofipap <popesolo88@gmail.com>
2026-05-09 20:03:59 +0000

    Add Files

--- server/server.js
+++ server/server.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const connectDB = require('./config/db');
 
 const app = express();
 
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
+// serve frontend folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
 app.use('/api/orders', require('./routes/orders'));
 
+// home page (THIS is what you asked about)
 app.get('/', (req, res) => {
-  res.send("Kofi's Pen API Running");
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// Start server FIRST
 app.listen(5000, "0.0.0.0", () => {
   console.log("Server running on port 5000");
 });
-
-// THEN connect DB (non-blocking)
-connectDB();
